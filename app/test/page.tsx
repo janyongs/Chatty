03c4392b2966d7bc9bce043b4d98b14f8e20608e
janyongs <wldud5023@naver.com>
2024-07-06 00:16:05 +0900

    feat: 음성인식 캐릭터 기본구현

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import VoiceMouth from "@/components/scene/VoiceMouth";
+
+export default function test() {
+  return (
+    <main className="w-screen h-screen p-8">
+      <div className="w-full h-screen flex items-center justify-center bg-slate-300 rounded-2xl">
+        <VoiceMouth />
+      </div>
+    </main>
+  );
+}
