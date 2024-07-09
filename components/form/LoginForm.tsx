4aa1b6390518227439c256fec2a366855d1d4b99
janyongs <wldud5023@naver.com>
2024-07-09 15:13:08 +0900

    feat: 로그인페이지 스타일구현

--- components/form/LoginForm.tsx
+++ components/form/LoginForm.tsx
@@ -3,7 +3,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { InputProps } from '../commons/Input';
 import Input from '../commons/Input';
-
+import Link from 'next/link';
 export const FieldNames = {
     email: 'email',
     password: 'password'
@@ -15,18 +15,26 @@ const fieldOptions: { [key in LoginTypeUnion]: InputProps } = {
     //각 input에 들어갈 props들을 정의합니다.
     email: {
         id: 'email',
-        label: '이메일',
+        label: 'Email',
         placeholder: '이메일을 입력하세요',
         validation: {
-            required: '이메일을 입력하세요!'
+            required: '이메일을 입력하세요!',
+            pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: '올바른 이메일을 입력해주세요.'
+            }
         }
     },
     password: {
         id: 'password',
-        label: '패스워드',
+        label: 'Password',
         placeholder: '비밀번호를 입력하세요',
         validation: {
-            required: '이메일을 입력하세요!'
+            required: '비밀번호를 잊으셨나요?',
+            pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                message: '올바른 비밀번호를 입력해주세요.'
+            }
         }
     }
 };
@@ -39,10 +47,21 @@ const LoginForm = () => {
     };
     return (
         <FormProvider {...form}>
-            <form onSubmit={handleOnSubmit}>
+            <form onSubmit={handleOnSubmit} className="w-full flex flex-col">
                 <Input {...fieldOptions.email} />
                 <Input {...fieldOptions.password} />
-                <button className="bg-red-500 p-4 text-white">로그인</button>
+
+                <div className="flex gap-2 mt-4 items-center justify-end">
+                    <Link
+                        href="/register"
+                        className="text-[#cc4e00] text-body1-regular w-1/4 rounded-lg p-3 bg-[#ff91014a] flex justify-center"
+                    >
+                        <button className="text-center">가입하기</button>
+                    </Link>
+                    <button className="bg-[#ef5f00] rounded-lg text-white w-1/4 text-body1-regular p-3">
+                        로그인
+                    </button>
+                </div>
             </form>
         </FormProvider>
     );
